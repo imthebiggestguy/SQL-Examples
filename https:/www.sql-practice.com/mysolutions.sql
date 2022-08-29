e10ee312fa6e80c3b3a4de609e5030acfce240a6
imthebiggestguy <107901643+imthebiggestguy@users.noreply.github.com>
2022-08-28 21:20:19 -0500

    Update mysolutions.sql

--- https:/www.sql-practice.com/mysolutions.sql
+++ https:/www.sql-practice.com/mysolutions.sql
@@ -1,3 +1,13 @@
+--Show all of the patients grouped into weight groups. Show the total amount of patients in each weight group. Order the list by the weight group decending.
+--For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
+SELECT count(patient_id) AS patients_in_group, FLOOR(weight / 10) * 10 AS weight_group
+FROM patients
+GROUP BY weight_group
+ORDER BY weight_group DESC
+;
+
+
+
 --Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
 SELECT first_name, last_name, allergies
 FROM patients
