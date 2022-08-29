b04550a6e7efe2ca7667b4ae69d468d4cb77c452
imthebiggestguy <107901643+imthebiggestguy@users.noreply.github.com>
2022-08-28 21:21:22 -0500

    Update mysolutions.sql

--- https:/www.sql-practice.com/mysolutions.sql
+++ https:/www.sql-practice.com/mysolutions.sql
@@ -8,7 +8,6 @@ ORDER BY weight_group DESC
 ;
 
 
-
 /*Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. 
 Show results ordered ascending by allergies then by first_name then by last_name.*/
 SELECT first_name, last_name, allergies
